be172d76f72ecd3b4566f460873e73fcc9d80a1b
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-05-15 09:28:50 -0400

    Ajouter une tâche programmée

--- src/main/java/ca/uqam/projet/tasks/PrintQuoteTask.java
+++ src/main/java/ca/uqam/projet/tasks/PrintQuoteTask.java
@@ -1,12 +1,20 @@
 package ca.uqam.projet.tasks;
 
+import ca.uqam.projet.repositories.*;
 import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.scheduling.annotation.*;
 
+@Component
 public class PrintQuoteTask {
 
   private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+  @Autowired CitationRepository repository;
+
+  @Scheduled(cron="* * * * * ?") // à toutes les secondes.
   public void printQuote() {
-    log.info("Insérer citation ici...");
+    log.info(repository.findById(1).getContenu());
   }
 }
