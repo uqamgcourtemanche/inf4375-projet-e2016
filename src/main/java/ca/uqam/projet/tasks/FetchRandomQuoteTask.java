0700bce6786c1483dc37e13bd448854158b16047
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2016-05-15 21:28:17 -0400

    Actually fetch quotes...

--- src/main/java/ca/uqam/projet/tasks/FetchRandomQuoteTask.java
+++ src/main/java/ca/uqam/projet/tasks/FetchRandomQuoteTask.java
@@ -15,8 +15,24 @@ import org.springframework.web.client.*;
 public class FetchRandomQuoteTask {
 
   private static final Logger log = LoggerFactory.getLogger(FetchRandomQuoteTask.class);
+  private static final String URL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=3";
 
   @Scheduled(cron="*/2 * * * * ?") // à toutes les 2 secondes.
   public void execute() {
+    Arrays.asList(new RestTemplate().getForObject(URL, RandomQuote[].class)).stream()
+      .map(this::asCitation)
+      .map(Citation::toString)
+      .forEach(log::info)
+      ;
   }
+
+  private Citation asCitation(RandomQuote q) {
+    return new Citation(q.id, Jsoup.parse(q.content).text(), q.author);
+  }
+}
+
+class RandomQuote {
+  @JsonProperty("ID") int id;
+  @JsonProperty("title") String author;
+  @JsonProperty("content") String content;
 }
